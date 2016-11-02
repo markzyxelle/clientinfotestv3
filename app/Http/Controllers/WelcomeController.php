2a0da1a7084bd16b47a2f7b9f42f71d2e24b2a11
markzyxelle <markzyxelle@gmail.com>
2016-11-02 09:08:17 +0800

    removed auth

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -30,7 +30,11 @@ class WelcomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('welcome');
+		// return view('welcome');
+
+		$arr = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+
+    	return json_encode($arr);
 	}
 
 }
