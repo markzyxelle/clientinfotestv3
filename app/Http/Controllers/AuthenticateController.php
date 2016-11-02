e32e47f09700f4d6d4f2ee331cc0cc20f198471a
markzyxelle <markzyxelle@gmail.com>
2016-11-02 15:17:00 +0800

    test return for authenticate

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -16,16 +16,17 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 class AuthenticateController extends Controller {
 
 	public function authenticate(Request $request){
-		$credentials = $request->only('username', 'password');
-		try{
-			if(!$token = JWTAuth::attempt($credentials)){
-				return response()->json(['error' => 'invalid_credentials'], 401);
-			}
-		} catch (JWTException $e){
-			return response()->json(['error' => 'could_not_create_token'], 500);
-		}
+		return "abcd";
+		// $credentials = $request->only('username', 'password');
+		// try{
+		// 	if(!$token = JWTAuth::attempt($credentials)){
+		// 		return response()->json(['error' => 'invalid_credentials'], 401);
+		// 	}
+		// } catch (JWTException $e){
+		// 	return response()->json(['error' => 'could_not_create_token'], 500);
+		// }
 
-		return response()->json(compact('token'));
+		// return response()->json(compact('token'));
 	}
 
 	/**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,4 +13,4 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::post('authenticate', 'AuthenticateController@authenticate');
+Route::get('authenticate', 'AuthenticateController@authenticate');
