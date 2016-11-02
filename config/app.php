d5bc5932d0a94a55e75eecf9f876ea307b99cf52
markzyxelle <markzyxelle@gmail.com>
2016-11-02 09:04:23 +0800

    installed JWT and CORS

--- config/app.php
+++ config/app.php
@@ -144,6 +144,8 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
+		'Tymon\JWTAuth\Providers\JWTAuthServiceProvider',
+		'Barryvdh\Cors\CorsServiceProvider',
 
 	],
 
@@ -192,6 +194,7 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
 
 	],
 
